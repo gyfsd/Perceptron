8446e93576d4500742dc5b82766a16a12c873ff5
gyfsd <92525133+gyfsd@users.noreply.github.com>
2023-12-27 18:42:54 +0300

    Delete PerceptronA/MeshLoader.h

--- PerceptronA/MeshLoader.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <Windows.h>
-#include <stdint.h>
-#include <iostream>
-#include <stdio.h>
-#include "3DObjects.h"	
-mesh3d LoadObjMesh(wchar_t* path);
-mesh3d LoadObjMeshA(char* path);
